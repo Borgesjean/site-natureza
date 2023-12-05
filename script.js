eedcdd11e01eafd066b32362a2d7c6d5c0e42e40
Borgesjean <jeancar7@gmail>
2023-12-05 12:37:56 -0400

    Projeto-Site

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header');
+    const container = document.getElementById('container');
+    const menuButton = document.getElementById('menu');
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    function handleScroll() {
+        container.classList.remove('menuopen');
+        header.classList.toggle('sticky', window.scrollY >= 100);
+    }
+
+    function handleMenuButtonClick() {
+        header.classList.remove('sticky');
+        container.classList.toggle('menuopen');
+    }
+
+    function handleLinkClick(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    }
+
+    function handleCloseOutside(event) {
+        if (!menuButton.contains(event.target)) {
+            container.classList.remove('menuopen');
+            header.classList.add('sticky');
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    menuButton.addEventListener('click', handleMenuButtonClick);
+    links.forEach(link => link.addEventListener('click', handleLinkClick));
+
+    document.addEventListener('click', handleCloseOutside);
+});
